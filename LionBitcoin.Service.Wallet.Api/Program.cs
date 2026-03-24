7b0daa9bcf479807be89f3af295b16005fbd13eb
WannabeNakamoto <wannabe_nakamoto@proton.me>
2026-03-24 20:55:53 +0400

    init

--- LionBitcoin.Service.Wallet.Api/Program.cs
+++ LionBitcoin.Service.Wallet.Api/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi("docs");
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapOpenApi();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/openapi/docs.json", "SharpStreamer.Api");
+});
 
+app.MapControllers();
 app.Run();
