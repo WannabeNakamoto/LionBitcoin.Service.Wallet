0b6e803ab219188abdc05f0c8d3d7d3c839e1330
WannabeNakamoto <wannabe_nakamoto@proton.me>
2026-03-30 21:30:14 +0400

    add migration

---
+++ client/LionBitcoin.Service.Wallet.Client.Persistence/Migrations/20260330172220_DepositAddressMigration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LionBitcoin.Service.Wallet.Client.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class DepositAddressMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "deposit_address",
+                table: "wallets",
+                type: "character varying(62)",
+                maxLength: 62,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "deposit_address",
+                table: "wallets");
+        }
+    }
+}
